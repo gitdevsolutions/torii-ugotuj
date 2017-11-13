0faf0a03e2fde90383084f57969b55937c7a5cb9
Mateusz Kulesza <m.kulesza@pjwstk.edu.pl>
2017-11-13 18:42:51 +0100

    Router, Forms, Http module imports

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,11 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpClient } from '@angular/common/http';
 
 
 import { AppComponent } from './app.component';
+import { Routing } from "./app.routing";
 
 
 @NgModule({
@@ -10,7 +13,10 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    HttpClient,
+    Routing
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/app.routing.ts
@@ -0,0 +1,9 @@
+import { RouterModule, Routes } from '@angular/router'
+
+const routes:Routes = [
+    
+]
+
+export const Routing = RouterModule.forRoot(routes,{
+    useHash: true
+})
